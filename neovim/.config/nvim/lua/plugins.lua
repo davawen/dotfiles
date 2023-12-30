df8ef8b8dd6bede533fe5059af68f426681685ed
davawen <davawen@proton.me>
2023-12-30 16:12:01 +0100

    add kitty scrollback buffer

--- neovim/.config/nvim/lua/plugins.lua
+++ neovim/.config/nvim/lua/plugins.lua
@@ -311,7 +311,7 @@ local plugins = {
 		end
 	},
 
-	-- Terminal
+	-- terminal
 	{ 'numToStr/FTerm.nvim',
 		config = function()
 			require('FTerm').setup {
@@ -330,6 +330,18 @@ local plugins = {
 			require('flatten').setup {}
 		end,
 		enabled = false
+	},
+	{
+		'mikesmithgh/kitty-scrollback.nvim',
+		enabled = true,
+		lazy = true,
+		cmd = { 'KittyScrollbackGenerateKittens', 'KittyScrollbackCheckHealth' },
+		event = { 'User KittyScrollbackLaunch' },
+		-- version = '*', -- latest stable version, may have breaking changes if major version changed
+		-- version = '^2.0.0', -- pin major version, include fixes and features that do not have breaking changes
+		config = function()
+			require('kitty-scrollback').setup()
+		end,
 	},
 
 	-- Git
