bce43fde29caf8c53d28a7d8c58da955d1a119f4
davawen <davawen@proton.me>
2025-10-30 15:37:30 +0100

    add tinymist and disable ts_ls lsp

--- neovim/.config/nvim/lua/lsp.lua
+++ neovim/.config/nvim/lua/lsp.lua
@@ -152,6 +152,8 @@ vim.g.markdown_fenced_languages = {
   "ts=typescript"
 }
 
+-- Disable ts ls by default (prefer deno)
+vim.lsp.enable("ts_ls", false)
 vim.lsp.enable("denols")
 
 -- vim.lsp.config.svelte.setup{
@@ -290,4 +292,5 @@ vim.lsp.config("lua_ls", {
 vim.lsp.enable({"clangd", "pyright", "omnisharp", "lua_ls"})
 vim.lsp.enable({"ols", "zls", "wgsl_analyzer", "glsl_analyzer", "lua_ls", "gopls", "julials"})
 vim.lsp.enable({"cssls", "html", "emmet_ls"})
+vim.lsp.enable({"tinymist"})
 
